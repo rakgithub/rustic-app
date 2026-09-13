0337bb601f77d029d71dcf333e553ede1ab3a2bf
Rakesh Shetty <rakeshshetty@Mac.fritz.box>
2026-09-13 14:08:47 +0200

    fix: fixed lint issues

--- libs/commerce/product-overview/product-overview.tsx
+++ libs/commerce/product-overview/product-overview.tsx
@@ -1,9 +1,6 @@
-import { useNavigate } from "react-router-dom";
 import { ProductList } from "product-list";
 
 function ProductOverview() {
-  const navigate = useNavigate();
-
   return (
     <section>
       <ProductList />
